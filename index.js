63a8d983d6be2f38ff60d87d0b95f0545fa340d7
Marko Puzovic <markopuzovic98@gmail.com>
2021-04-29 00:43:04 +0200

    Add scoreboard category and discipline

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const url = require("url");
 const querystring = require("querystring");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "fitness_competition",
+const connection = mysql.createPool({
+  host: "eu-cdbr-west-01.cleardb.com",
+  user: "bc3bee78fedd1c",
+  password: "99b2122c",
+  database: "heroku_221d49f6c99d98e",
 });
-connection.connect(() => {});
 const app = express();
 app.listen(process.env.PORT || 8080);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/api/categories", (req, res) => {
   connection.query("SELECT * FROM category", (err, result) => {
-    if (err) throw err;
+    if (err) console.error(err);
     res.write(JSON.stringify(result));
     res.end();
   });
@@ -28,7 +27,7 @@ app.get("/api/disciplines", (req, res) => {
   if (req.query.category != undefined)
     querystring = `SELECT discipline.id,discipline.name as discipline,discipline.first_place,discipline.second_place,discipline.third_place,discipline_type.name,discipline_type.unit from discipline,discipline_type,discipline_category,category where discipline_typeID=discipline_type.id and discipline_category.categoryID=category.id and discipline_category.disiplineID=discipline.id and category.id=${req.query.category}`;
   connection.query(querystring, (err, result) => {
-    if (err) throw err;
+    if (err) console.error(err);
     res.write(JSON.stringify(result));
     res.end();
   });
@@ -45,7 +44,7 @@ app.get("/api/competitors", (req, res) => {
       : ``
   }`;
   connection.query(querystring, (err, result) => {
-    if (err) throw err;
+    if (err) console.error(err);
     res.write(JSON.stringify(result));
     res.end();
   });
@@ -54,7 +53,7 @@ app.get("/api/competitors", (req, res) => {
 app.get("/api/competitors/scoreboard", (req, res) => {
   let querystring = `SELECT concat(competitor.first_name,' ',competitor.last_name),SUM(competes.points)  from competitor,competes,discipline where competes.disciplineID=discipline.id and competes.competitorID=competitor.id and competitor.categoryID=${req.query.category} GROUP BY concat(competitor.first_name,' ',competitor.last_name) `;
   connection.query(querystring, (err, result) => {
-    if (err) throw err;
+    if (err) console.error(err);
     res.write(JSON.stringify(result));
     res.end();
   });
