886b0a5d0a1c1c47b8ff49b2ba4484fc49ce1c52
Marko Puzovic <markopuzovic98@gmail.com>
2021-05-04 10:07:42 +0200

    Add socket.io and sync

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-const url = require("url");
-const querystring = require("querystring");
 const connection = mysql.createPool({
   host: "eu-cdbr-west-01.cleardb.com",
   user: "bc3bee78fedd1c",
   password: "99b2122c",
   database: "heroku_221d49f6c99d98e",
+  multipleStatements: true,
 });
 const app = express();
-app.listen(process.env.PORT || 8080);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("Neko se konektovao ");
+});
+
+server.listen(process.env.PORT || 8080);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
@@ -42,7 +48,7 @@ app.get("/api/disciplines", (req, res) => {
 });
 
 app.get("/api/competitors", (req, res) => {
-  let querystring = `SELECT competitor.id as id,concat(competitor.first_name,' ',competitor.last_name) as name,concat(competes.quantity,' ',discipline_type.unit) as points from competitor,competes,discipline,discipline_type where discipline_type.id=discipline.discipline_typeID AND competes.disciplineID=discipline.id and competes.competitorID=competitor.id ${
+  let querystring = `SELECT category.name as category, competes.id as compID,discipline.name as discipline,discipline_type.name as type, competitor.id as id,concat(competitor.first_name,' ',competitor.last_name) as name,concat(competes.quantity,' ',discipline_type.unit) as points, competes.quantity as quantity from competitor,competes,discipline,discipline_type,category where category.id=competitor.categoryID and discipline_type.id=discipline.discipline_typeID AND competes.disciplineID=discipline.id and competes.competitorID=competitor.id ${
     req.query.discipline != undefined
       ? `AND discipline.id=${req.query.discipline}`
       : ``
@@ -50,7 +56,12 @@ app.get("/api/competitors", (req, res) => {
     req.query.category != undefined
       ? `AND competitor.categoryID=${req.query.category}`
       : ``
+  } ${
+    req.query.filter != undefined
+      ? ` AND concat(competitor.first_name,' ',competitor.last_name) LIKE '%${req.query.filter}%'`
+      : ``
   }`;
+  querystring += " ORDER BY competes.quantity DESC";
   connection.query(querystring, (err, result) => {
     if (err) console.error(err);
     res.write(JSON.stringify(result));
@@ -59,7 +70,7 @@ app.get("/api/competitors", (req, res) => {
 });
 
 app.get("/api/competitors/scoreboard", (req, res) => {
-  let querystring = `SELECT competitor.id as id,concat(competitor.first_name,' ',competitor.last_name) as name,SUM(competes.points) as points   from competitor,competes,discipline where competes.disciplineID=discipline.id and competes.competitorID=competitor.id and competitor.categoryID=${req.query.category} GROUP BY concat(competitor.first_name,' ',competitor.last_name) ORDER BY points DESC `;
+  let querystring = `SELECT competitor.id as id,concat(competitor.first_name,' ',competitor.last_name) as name,SUM(competes.points) as points   from competitor,competes,discipline where competes.disciplineID=discipline.id and competes.competitorID=competitor.id and competitor.categoryID=${req.query.category} GROUP BY competitor.id,concat(competitor.first_name,' ',competitor.last_name) ORDER BY points DESC `;
   connection.query(querystring, (err, result) => {
     if (err) console.error(err);
     res.write(JSON.stringify(result));
@@ -90,6 +101,8 @@ app.post("/api/competitors", (req, res) => {
               categories: result,
             })
           );
+
+          io.emit("refresh", insertedResult.insertId);
           res.end();
         }
       );
@@ -98,6 +111,7 @@ app.post("/api/competitors", (req, res) => {
 });
 app.post("/api/competes", (req, res) => {
   const { competitor, disciplines } = req.body;
+  console.log(`discplines`, competitor);
   let values = [];
   disciplines.forEach((discipline) => {
     values.push([null, discipline, competitor, 0, 0]);
@@ -106,9 +120,25 @@ app.post("/api/competes", (req, res) => {
     if (err) console.error(err);
     else {
       console.log(`result`, result);
+      io.emit("refresh", competitor);
       res.end();
     }
   });
+});
+
+app.post("/api/results", (req, res) => {
+  const { competesID, result, competitor } = req.body;
+  let querystring = `UPDATE competes set quantity=? where id=?;SET @sum = (SELECT SUM(competes.quantity) from competitor,competes,discipline,discipline_type where competitor.id=competes.competitorID and competes.disciplineID=discipline.id and discipline.discipline_typeID=discipline_type.id AND discipline_type.name='ORM' and competitor.id=?);SELECT @sum;UPDATE competes SET competes.quantity=@sum where competitorID=? AND disciplineID=(SELECT id from discipline where name='TOTAL');`;
+  connection.query(
+    querystring,
+    [parseFloat(result), competesID, competitor, competitor],
+    (err, result) => {
+      if (err) console.error(err);
+      console.log(`result`, result);
+      io.emit("refresh", competitor);
+      res.end();
+    }
+  );
 });
 /*
 
